a48620b9ac57eb44b877c6961c421d8056eb49e1
Vi Le <lecongvi18052002@gmail.com>
2024-10-31 15:44:43 +0700

    Add location on /v1/locations

---
+++ WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.skyapi.weatherforecast.location;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.skyapi.weatherforecast.common.Location;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class LocationRepositoryTests {
+
+	@Autowired
+	private LocationRepository repository;
+	
+	@Test
+	public void testAddSuccess() {
+		Location location = new Location();
+		location.setCode("NYC_USA");
+		location.setCityName("New York City");
+		location.setCountryCode("US");
+		location.setRegionName("New York");
+		location.setCountryName("United States of America");
+		location.setEnabled(true);
+		
+		Location savedLocation = repository.save(location);
+		assertThat(savedLocation).isNotNull();
+		assertThat(savedLocation.getCode()).isEqualTo("NYC_USA");
+	}
+}
