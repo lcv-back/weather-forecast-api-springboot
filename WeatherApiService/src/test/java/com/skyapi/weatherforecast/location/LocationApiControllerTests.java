7b9dae0774a5b0e1786fbe54fbc0078ea2895a89
Vi Le <lecongvi18052002@gmail.com>
2024-11-01 20:16:00 +0700

    Function get the list locations on dbs on status 200 TEST DONE: Completed

--- WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationApiControllerTests.java
+++ WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationApiControllerTests.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -56,13 +57,54 @@ public class LocationApiControllerTests {
 		
 		String bodyContent = mapper.writeValueAsString(location);
 		
-		mockMvc.perform(post(END_POINT_PATH).contentType("application/json").content(bodyContent)).andExpect(status().isCreated()).andExpect(content().contentType("application/json")).andExpect(jsonPath("$.code", is("NYC_USA"))).andExpect(jsonPath("$.city_name", is("New York City"))).andExpect(header().string("Location", "/v1/locations/NYC_USA")).andDo(print());
+		mockMvc.perform(post(END_POINT_PATH)
+			.contentType("application/json")
+			.content(bodyContent))
+			.andExpect(status().isCreated())
+			.andExpect(content().contentType("application/json"))
+			.andExpect(jsonPath("$.code", is("NYC_USA")))
+			.andExpect(jsonPath("$.city_name", is("New York City")))
+			.andExpect(header().string("Location", "/v1/locations/NYC_USA"))
+			.andDo(print());
 	}
 	
 	@Test
 	public void testListShouldReturn204NoContent() throws Exception {
 		Mockito.when(service.list()).thenReturn(Collections.emptyList());
 		
-		mockMvc.perform(get(END_POINT_PATH)).andExpect(status().isNoContent()).andDo(print());
+		mockMvc.perform(get(END_POINT_PATH))
+			.andExpect(status().isNoContent())
+			.andDo(print());
+	}
+	
+	@Test
+	public void testListShouldReturn200OK() throws Exception {
+		Location location1 = new Location();
+		location1.setCode("NYC_USA");
+		location1.setCityName("New York City");
+		location1.setCountryCode("US");
+		location1.setRegionName("New York");
+		location1.setCountryName("United States of America");
+		location1.setEnabled(true);
+		
+		Location location2 = new Location();
+		location2.setCode("HaNoi_VI");
+		location2.setCityName("Ha Noi City");
+		location2.setCountryCode("VI");
+		location2.setRegionName("Ha Noi");
+		location2.setCountryName("Viet Nam");
+		location2.setEnabled(true);
+		
+		Mockito.when(service.list()).thenReturn(List.of(location1, location2));
+		
+		mockMvc.perform(get(END_POINT_PATH))
+			.andExpect(status().isOk())
+			.andExpect(content()
+			.contentType("application/json"))
+			.andExpect(jsonPath("$[0].code", is("NYC_USA")))
+			.andExpect(jsonPath("$[0].city_name", is("New York City")))
+			.andExpect(jsonPath("$[1].code", is("HaNoi_VI")))
+			.andExpect(jsonPath("$[1].city_name", is("Ha Noi City")))
+			.andDo(print());
 	}
 }
