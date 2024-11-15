b27696b36f233fff21f096d506ab609aa6d28ce6
Vi Le <lecongvi18052002@gmail.com>
2024-11-15 15:26:42 +0700

    UNIT TEST: Get realtime weather on status 200 Ok DONE

--- WeatherApiService/src/test/java/com/skyapi/weatherforecast/realtime/RealtimeWeatherApiControllerTests.java
+++ WeatherApiService/src/test/java/com/skyapi/weatherforecast/realtime/RealtimeWeatherApiControllerTests.java
@@ -2,7 +2,10 @@ package com.skyapi.weatherforecast.realtime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,6 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skyapi.weatherforecast.GeolocationException;
 import com.skyapi.weatherforecast.GeolocationService;
 import com.skyapi.weatherforecast.common.Location;
+import com.skyapi.weatherforecast.common.RealtimeWeather;
 import com.skyapi.weatherforecast.location.LocationNotFoundException;
 
 @WebMvcTest(RealtimeWeatherApiController.class)
@@ -46,6 +50,36 @@ public class RealtimeWeatherApiControllerTests {
 		
 		mockMvc.perform(get(END_POINT_PATH))
 			.andExpect(status().isNotFound())
+			.andDo(print());
+	}
+	
+	@Test
+	public void testGetShouldReturnStatus200OK() throws Exception {
+		Location location = new Location();
+		location.setCode("HCM_VI");
+		location.setCityName("Ho Chi Minh City");
+		location.setRegionName("Ho Chi Minh");
+		location.setCountryName("Viet Nam");
+		location.setCountryCode("VI");
+		
+		RealtimeWeather realtimeWeather = new RealtimeWeather();
+		realtimeWeather.setHumidity(32);
+		realtimeWeather.setLocation(location);
+		realtimeWeather.setLastUpdated(new Date());
+		realtimeWeather.setTemperature(12);
+		realtimeWeather.setStatus("Cloudy");
+		realtimeWeather.setWindSpeed(5);
+		realtimeWeather.setPrecipitation(88);
+		
+		location.setRealtimeWeather(realtimeWeather);
+		
+		Mockito.when(locationService.getLocation(Mockito.anyString())).thenReturn(location);
+		Mockito.when(realtimeWeatherService.getByLocation(location)).thenReturn(realtimeWeather);
+		
+		mockMvc.perform(get(END_POINT_PATH))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType("application/json"))
+			
 			.andDo(print());
 	}
 }
