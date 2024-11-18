3b11e27f37e4bcbfe27abdeb57cdbc993f9e208d
Vi Le <lecongvi18052002@gmail.com>
2024-11-18 16:24:11 +0700

    UNIT TEST: Delete hourly weather for the location by the given hour of day DONE

--- WeatherApiService/src/test/java/com/skyapi/weatherforecast/hourly/HourlyWeatherRepositoryTests.java
+++ WeatherApiService/src/test/java/com/skyapi/weatherforecast/hourly/HourlyWeatherRepositoryTests.java
@@ -3,6 +3,8 @@ package com.skyapi.weatherforecast.hourly;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
 import com.skyapi.weatherforecast.common.HourlyWeather;
+import com.skyapi.weatherforecast.common.HourlyWeatherId;
 import com.skyapi.weatherforecast.common.Location;
 
 @DataJpaTest
@@ -22,7 +25,7 @@ public class HourlyWeatherRepositoryTests {
 	private HourlyWeatherRepository repo;
 	
 	@Test
-	public void testAdd() {
+	public void testAddHourlyWeather() {
 		String locationCode = "HCM_VI";
 		int hourOfDay = 12;
 		
@@ -39,5 +42,19 @@ public class HourlyWeatherRepositoryTests {
 	
 		assertThat(updatedForecast.getId().getLocation().getCode().equals(locationCode));
 		assertEquals(updatedForecast.getId().getHourOfDay(), hourOfDay);
+	}
+	
+	@Test
+	public void testDeleteHourlyWeather() {
+		String locationCode = "HCM_VI";
+		Location location = new Location().code(locationCode);
+		
+		HourlyWeatherId id = new HourlyWeatherId(8, location);
+		
+		repo.deleteById(id);
+		
+		Optional<HourlyWeather> result = repo.findById(id);
+		
+		assertThat(result).isNotPresent();
 	}
 }	
